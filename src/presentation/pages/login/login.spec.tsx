22bb0f78efbdaa0fdd552b95f57c76fbebce8c2b
felipeferrarini <ferrarinifelipe@gmail.com>
2022-03-02 00:31:02 -0300

    refactor: change validation interface

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -15,10 +15,12 @@ type SutTypes = {
 
 class ValidationSpy implements Validation {
   errorMessage: string
-  input: object
+  fieldName: string
+  fieldValue: string
 
-  validate(input: object): string {
-    this.input = input
+  validate(fieldName: string, fieldValue: string): string {
+    this.fieldName = fieldName
+    this.fieldValue = fieldValue
     return this.errorMessage
   }
 }
@@ -59,9 +61,8 @@ describe('Login component', () => {
 
     const emailInput = sut.getByTestId('email') as HTMLInputElement
     fireEvent.input(emailInput, { target: { value: 'any_email' } })
-    expect(validationSpy.input).toEqual({
-      email: 'any_email'
-    })
+    expect(validationSpy.fieldName).toBe('email')
+    expect(validationSpy.fieldValue).toBe('any_email')
   })
 
   test('should call Validation with correct password', () => {
@@ -69,8 +70,7 @@ describe('Login component', () => {
 
     const passwordInput = sut.getByTestId('password') as HTMLInputElement
     fireEvent.input(passwordInput, { target: { value: 'any_password' } })
-    expect(validationSpy.input).toEqual({
-      password: 'any_password'
-    })
+    expect(validationSpy.fieldName).toBe('password')
+    expect(validationSpy.fieldValue).toBe('any_password')
   })
 })
